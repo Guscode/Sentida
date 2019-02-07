b5327e052d39e9141d58be028679fde25f5a4d7a
Guscode <35924673+Guscode@users.noreply.github.com>
2019-02-07 13:21:32 +0100

    Update Function.R

--- R/Function.R
+++ R/Function.R
@@ -25,9 +25,9 @@ Function <- function(string){
   string <- str_replace_all(string, "[[:punct:]]", "")
   if (" " %in% space){
     realstr <- strsplit(string, " ")[[1]]
-  } else {
+    } else {
     realstr <- string
-  }
+    }
   if ("!" %in% space){
     intense <- 1.29
   }
@@ -36,29 +36,30 @@ Function <- function(string){
     if (word == "ikk"){
       rev <- 3
     }
+    if (word %in% intensifier$stem){
+        mul <- 2
+        multiplier <- intensifier$score[which(intensifier$stem == word)]
+      }
     if (word %in% aarup$stem){
       wordsc <-  aarup$score[which(aarup$stem == word)]
       if (rev == 1 | rev == 2){
         wordsc <- wordsc*(-1)
       }
-     if (word %in% intensifier$stem){
-      mul <- 2
-      multiplier <- intensifier$score[which(intensifier$stem == word)]
-    }
+      
       if (mul == 1){
         wordsc <- wordsc*multiplier
       }
-      score <- score+wordsc
       
+      score <- score+wordsc
     } else {
       score <- score + 0
     }
-    if (rev != 0){
-      rev <- rev-1 
-    }
-    if (mul != 0){
-      mul <- mul - 1
-    }
+  if (rev != 0){
+    rev <- rev-1 
+  }
+  if (mul != 0){
+    mul <- mul - 1
+  }
   }
   return(score*intense)
 }
